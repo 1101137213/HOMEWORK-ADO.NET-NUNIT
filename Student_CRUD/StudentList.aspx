f37801bb2953437dfdfd1b645f1e50965224e27d
1101137213 <scientologistwlh@gmail.com>
2015-03-29 23:41:34 +0800

    First Finished

---
+++ Student_CRUD/StudentList.aspx
@@ -0,0 +1,101 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="StudentList.aspx.cs" Inherits="Student_CRUD.StudentList" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>學生基本資料</title>
+    <meta charset="utf-8">
+    <link rel="shortcut icon" href="http://www.veryicon.com/icon/png/System/Fruity%20Apples/Seablue%20512.png">
+    <link rel="stylesheet" href="./css/basicStyle.css">
+
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                <td><asp:TextBox ID="textbox_name_box" runat="server" Class="queryBox box" placeholder="以姓名查詢"></asp:TextBox></td>
+                <td><asp:Button ID="button_do_query" OnClick="btnQuery_Click" runat="server" Text="查詢" Class="queryButton button" /></td>
+                </tr>
+            </table>
+            <br /><br />
+
+            <asp:GridView ID="grid_view_student" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None">
+                <AlternatingRowStyle BackColor="White" />
+                <EditRowStyle BackColor="#7C6F57" />
+                <FooterStyle BackColor="#1C5E55" Font-Bold="True" ForeColor="White" />
+                <HeaderStyle BackColor="#1C5E55" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="#666666" ForeColor="White" HorizontalAlign="Center" />
+                <RowStyle BackColor="#E3EAEB" />
+                <SelectedRowStyle BackColor="#C5BBAF" Font-Bold="True" ForeColor="#333333" />
+                <SortedAscendingCellStyle BackColor="#F8FAFA" />
+                <SortedAscendingHeaderStyle BackColor="#246B61" />
+                <SortedDescendingCellStyle BackColor="#D4DFE1" />
+                <SortedDescendingHeaderStyle BackColor="#15524A" />
+            </asp:GridView>
+
+            
+            <br />
+            <table>
+                <tr>
+                    <td>
+                        <asp:Label ID="label_name_insert" runat="server" Text="姓名" ></asp:Label>
+                        <asp:TextBox ID="textbox_name_insert" runat="server" Class="insertBox box"></asp:TextBox>
+                    </td>
+
+                    <td>
+                        <asp:Label ID="label_birthday_insert" runat="server" Text="生日"></asp:Label>
+                        <asp:TextBox ID="textbox_birthday_insert" runat="server" Class="insertBox box" placeholder="格式：yyyy/mm/dd"></asp:TextBox>
+                    </td>
+
+                    <td>
+                        <asp:Label ID="label_information_insert" runat="server" Text="備註"></asp:Label>
+                        <asp:TextBox ID="textbox_information_insert" runat="server" Class="insertBox box" placeholder="可填可不填"></asp:TextBox>
+                    </td>
+
+                    <td colspan="2">
+                        <asp:Button ID="button_do_insert" runat="server" Text="新增" OnClick="button_do_insert_Click" class="insertButton button"/>
+                    </td>
+                </tr>
+
+                <tr>
+                     <td>
+                        <asp:Label ID="label_id_update" runat="server" Text="學號"></asp:Label>
+                        <asp:TextBox ID="textbox_id_update" runat="server" Class="updateBox box" placeholder="輸入要修改的學號"></asp:TextBox>
+                     </td>
+
+                     <td>
+                        <asp:Label ID="label_name_update" runat="server" Text="姓名"></asp:Label>
+                        <asp:TextBox ID="textbox_name_update" runat="server" Class="updateBox box"></asp:TextBox>
+                    </td>
+
+                    <td>
+                        <asp:Label ID="label_birthday_update" runat="server" Text="生日"></asp:Label>
+                        <asp:TextBox ID="textbox_birthday_update" runat="server" Class="updateBox box" placeholder="格式：yyyy/mm/dd"></asp:TextBox>
+                    </td>
+                    
+                    <td>
+                        <asp:Label ID="label_information_update" runat="server" Text="備註"></asp:Label>
+                        <asp:TextBox ID="textbox_information_update" runat="server" Class="updateBox box" placeholder="可填可不填"></asp:TextBox>
+                    </td>
+
+                    <td>
+                        <asp:Button ID="button_do_update" runat="server" Text="修改" OnClick="button_do_update_Click" class="updateButton button"/>
+                    </td>
+            
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="label_id_delete" runat="server" Text="編號"></asp:Label>
+                        <asp:TextBox ID="textbox_id_delete" runat="server" Class="deleteBox box" placeholder="輸入要刪除的學號"></asp:TextBox>
+                    </td>
+                    <td colspan="4">
+                        <asp:Button ID="button_do_delete" runat="server" Text="刪除" OnClick="button_do_delete_Click" class="deleteButton button"/>
+                    </td>
+                </tr>
+        </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Student_CRUD/StudentList.aspx.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Student_CRUD
+{
+    public partial class StudentList : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            btnQuery_Click(null, null);// indentifier need to be changed
+        }
+
+
+
+        // using is required
+        protected void btnQuery_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["GSS_HWConnectionString"].ConnectionString))// this would be change too
+            {
+                using (SqlCommand cmd = cn.CreateCommand())
+                {
+                    cmd.CommandText = "select * from student where student_name like @name";// SQL Need to be modified
+                    cmd.Parameters.Add(new SqlParameter("@name", "%" + textbox_name_box.Text + "%"));
+
+                    using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                    {
+                        DataSet ds = new DataSet();
+                        da.Fill(ds);
+                        grid_view_student.DataSource = ds;
+                        grid_view_student.DataMember = "Table";
+                        grid_view_student.DataBind();
+                        da.Fill(ds);
+
+                    }
+                }
+            }
+        }
+
+        // using requery to do the ajax
+        protected void button_do_insert_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["GSS_HWConnectionString"].ConnectionString))
+            {
+                using (SqlCommand cmd = cn.CreateCommand())
+                {
+                    cmd.CommandText = "select * from student";
+                    cmd.Parameters.Add(new SqlParameter("@name", "%" + textbox_name_box.Text + "%"));// SQL Need to be modified
+
+                    using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                    {
+                        //自動產生Insert, Update, DeleteCommand
+                        SqlCommandBuilder builder = new SqlCommandBuilder(da);
+                        //builder.GetInsertCommand()
+                        //builder.GetUpdateCommand()
+                        //builder.GetDeleteCommand()
+
+                        DataSet ds = new DataSet();
+                        ds.Clear();
+                        da.Fill(ds);
+                        DataTable dt = ds.Tables["Table"];
+                        DataRow dr = dt.NewRow();
+                        // field to add
+
+                        dr["student_name"] = textbox_name_insert.Text;
+                        dr["studnet_birthday"] = textbox_birthday_insert.Text;
+                        dr["student_information"] = textbox_information_insert.Text;
+                        dt.Rows.Add(dr);
+                        da.Update(dt);
+
+                        // clear field
+                        textbox_name_insert.Text = "";
+                        textbox_birthday_insert.Text = "";
+                        textbox_information_insert.Text = "";
+
+                        btnQuery_Click(null, null);// why?
+
+
+                    }
+                }
+            }
+        }
+
+        protected void button_do_update_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["GSS_HWConnectionString"].ConnectionString))
+            {
+                using (SqlCommand cmd = cn.CreateCommand())
+                {
+                    cmd.CommandText = "select * from student";
+
+                    using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                    {
+                        //自動產生Insert, Update, DeleteCommand
+                        SqlCommandBuilder builder = new SqlCommandBuilder(da);
+                        //builder.GetInsertCommand()
+                        //builder.GetUpdateCommand()
+                        //builder.GetDeleteCommand()
+
+                        DataSet ds = new DataSet();
+                        ds.Clear();
+                        da.Fill(ds);
+                        DataTable dt = ds.Tables[0];
+                        DataRow dr = dt.Select(string.Format("student_id = {0}", textbox_id_update.Text)).First();
+                        dr["student_name"] = textbox_name_update.Text;
+                        dr["studnet_birthday"] = textbox_birthday_update.Text;
+                        dr["student_information"] = textbox_information_update.Text;
+                        da.Update(dt);
+
+                        // clear field
+                        textbox_name_update.Text = "";
+                        textbox_birthday_update.Text = "";
+                        textbox_information_update.Text = "";
+
+                        btnQuery_Click(null, null);// why?
+                    }
+                }
+            }
+        }
+
+        protected void button_do_delete_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["GSS_HWConnectionString"].ConnectionString))
+            {
+                using (SqlCommand cmd = cn.CreateCommand())
+                {
+                    cmd.CommandText = "select * from student";
+
+                    using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                    {
+                        //自動產生Insert, Update, DeleteCommand
+                        SqlCommandBuilder builder = new SqlCommandBuilder(da);
+                        //builder.GetInsertCommand()
+                        //builder.GetUpdateCommand()
+                        //builder.GetDeleteCommand()
+
+                        DataSet ds = new DataSet();
+                        ds.Clear();
+                        da.Fill(ds);
+                        DataTable dt = ds.Tables[0];
+                        DataRow dr = dt.Select(string.Format("student_id = {0}", textbox_id_delete.Text)).First();
+                        dr.Delete();
+                        da.Update(dt);
+
+                        // clear field
+                        textbox_id_delete.Text = "";
+
+                        btnQuery_Click(null, null);// why?
+                    }
+                }
+            }
+        }
+
+
+
+    }
+}
