69ac9ea7ab03c5a3c5f1a1a44fa270e591eb969a
1101137213 <scientologistwlh@gmail.com>
2015-04-10 15:27:43 +0800

    first

---
+++ WebApplication1.Tests/Class2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace WebApplication1.Tests
+{
+    [TestFixture]
+    public class Class2
+    {
+        [Test]
+        public void CalYear()
+        {
+            BO.Cal cal = new BO.Cal();
+            int inputAge = 30;
+            //int expect = 1988; this is false
+            int expect = 1985;//this is true
+
+            Assert.AreEqual(expect, cal.GetBirthYear(inputAge, BO.Cal.YearFormat.W));
+        }
+    }
+}
